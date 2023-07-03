d8d2a8c9723ece147bbe944caa5a7e370573dafc
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-07-03 16:57:14 +0900

    TabBar setting

---
+++ WhereSeat_iOS/WhereSeat_iOS/CalendarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarViewController.swift
+//  WhereSeat_iOS
+//
+//  Created by Phil on 2023/07/03.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
