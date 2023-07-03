972a15bdea2f05b9d9d414eb64162dee1ea153b3
pingu00 <58204852+pingu00@users.noreply.github.com>
2023-07-03 18:55:20 +0900

    swiftUI Hosting 연결

---
+++ WhereSeat_iOS/WhereSeat_iOS/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WhereSeat_iOS
+//
+//  Created by Phil on 2023/07/03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
--- WhereSeat_iOS/WhereSeat_iOS/HomeViewController.swift
+++ WhereSeat_iOS/WhereSeat_iOS/HomeViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
 
-
+    @IBSegueAction func embededSwiftUi(_ coder: NSCoder) -> UIViewController? {
+        return UIHostingController(coder: coder, rootView: HomeView())
+        
+    }
+    
 }
 
